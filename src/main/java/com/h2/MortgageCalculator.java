ca6eccf076093a94c956eed3a785f6630e6716f2
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-21 15:41:29 -0600

    create calculate monthly payment and main methods

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,56 @@
+package com.h2;
+
+import java.text.DecimalFormat;
+
+
+public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+
+    public MortgageCalculator (long loanAmount, int termInYears, float annualRate){
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
+
+    private int getNumberOfPayments() {
+       return termInYears * 12;
+    }
+
+    private float getMonthlyInterestRate() {
+        float interestRate = annualRate / 100;
+        float result = interestRate / 12;
+        return result;
+
+    }
+
+    public void calculateMonthlyPayment() {
+        long P = loanAmount;
+        float r = getMonthlyInterestRate();
+        int n = getNumberOfPayments();
+
+        double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+        M = this.monthlyPayment;
+    }
+
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("####0.00");
+       return "monthly payment: " + df.format(monthlyPayment);
+    }
+
+    public static void main(String[] args) {
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        final MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
+    }
+
+
+
+
+}
