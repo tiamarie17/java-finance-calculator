a12aebffe92c24296b30255d085fb5eba691a9c7
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-21 16:42:43 -0600

    create switch statement for executeCommand method

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,5 +1,9 @@
 
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator;
 import java.util.Map;
+import com.h2.BestLoanRates;
 public class Finance {
     public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
@@ -11,11 +15,42 @@ public class Finance {
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
-//    private static boolean validateCommandArguments(String[] args){
-//        switch(args[0]){
-//
-//
-//        }
-//        return
-//    }
-//}
+    private static boolean validateCommandArguments(String[] args){
+        switch(args[0]){
+            case BEST_LOAN_RATES:
+            return args.length == 1;
+
+            case SAVINGS_CALCULATOR:
+            return args.length ==2;
+
+            case MORTGAGE_CALCULATOR:
+            return args.length ==3;
+        }
+        return false;
+    }
+
+    private static void executeCommand(String command, String[] arguments){
+        switch(command){
+            case BEST_LOAN_RATES:
+            System.out.println("Finding the best loan rates...");
+            BestLoanRates.main(arguments);
+            return;
+
+            case SAVINGS_CALCULATOR:
+            System.out.println("Finding your net savings...");
+            SavingsCalculator.main(arguments);
+            return;
+
+            case MORTGAGE_CALCULATOR:
+            System.out.println("Finding your monthly payment...");
+            MortgageCalculator.main(arguments);
+            return;
+
+            }
+
+        }
+
+    public static void main (String[] args) {
+        String command = args[0];
+    }
+}
