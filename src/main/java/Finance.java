0f21c70aa3ed74aa8368d07f9a01717dab4985c2
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-21 19:15:41 -0600

    fix error on executeCommand

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -34,12 +34,12 @@ public class Finance {
     private static void executeCommand(String command, String[] arguments){
         switch(command){
             case BEST_LOAN_RATES:
-            System.out.println("Finding the best loan rates...");
+            System.out.println("Finding best loan rates ...");
             BestLoanRates.main(arguments);
             return;
 
             case SAVINGS_CALCULATOR:
-            System.out.println("Finding your net savings ...");
+            System.out.println("Finding your net savings...");
             SavingsCalculator.main(arguments);
             return;
 
