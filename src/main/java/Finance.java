16b6b8c468fa8d62d96fdd21f9f2dd44133a8b1c
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-21 19:12:58 -0600

    update public static fields in Finance class

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -2,14 +2,16 @@
 import com.h2.BestLoanRates;
 import com.h2.MortgageCalculator;
 import com.h2.SavingsCalculator;
-import java.util.Map;
-import com.h2.BestLoanRates;
-public class Finance {
-    public final static String BEST_LOAN_RATES = "bestLoanRates";
-    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
-    public final static String MORTGAGE_CALCULATOR = "mortageCalculator";
 
-    public final Map<String, String> commandsToUsage = Map.of(
+import java.util.Map;
+import java.util.Arrays;
+
+public class Finance {
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
@@ -21,10 +23,10 @@ public class Finance {
             return args.length == 1;
 
             case SAVINGS_CALCULATOR:
-            return args.length ==2;
+            return args.length ==3;
 
             case MORTGAGE_CALCULATOR:
-            return args.length ==3;
+            return args.length ==4;
         }
         return false;
     }
@@ -52,6 +54,16 @@ public class Finance {
 
     public static void main (String[] args) {
         String command = args[0];
+        if(!commandsToUsage.containsKey(command)) {
+            System.out.println(command + ": command not found");
+            return;
+        }
+        boolean isValidCommand = validateCommandArguments(args);
+        if(!isValidCommand){
+            System.out.println(commandsToUsage.get(args[0]));
+            return;
+        }
+        executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
 
     }
 }
