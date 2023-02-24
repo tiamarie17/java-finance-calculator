7ec79b12d114b4f3510e70ebeffd2a6cf9a21857
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-24 13:55:55 -0600

    update App to allow user to select which tool they want to use when main App runs

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,11 +1,36 @@
 package com.h2;
 
-public class App 
+import java.util.Scanner;
+import com.h2.BestLoanRates;
+import com.h2.SavingsCalculator;
+import com.h2.MortgageCalculator;
+import com.h2.Utilities;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select a service: Enter 'bestLoanRates', 'savingsCalculator', or 'mortgageCalculator'.");
+        String[] service = new String[]{scanner.nextLine()};
+        if (service[0].equals( "bestLoanRates")) {
+            System.out.println("Enter your loan term in years");
+            int loanTermInYears = scanner.nextInt();
+            float result = BestLoanRates.getRates(loanTermInYears);
+            System.out.println("Your rate is " + result);
+
+//        }else if (service[0] == "savingsCalculator") {
+//            SavingsCalculator.main();
+//        }else if (service[0] == "mortgageCalculator") {
+//            MortgageCalculator.main();
+//        }
+//        else {
+//            System.out.println("Did not receive a valid service. Exiting program.");
+//        }
+            scanner.close();
+        }
     }
+
+
 
     public static int doubleTheNumber(int number) {
         return (number * 2) ;
